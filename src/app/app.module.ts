432ea8082086559fe99cf2c7f4ee01935063c2cc
DaniGadget18 <dani_181298@hotmail.com>
2020-05-26 23:58:05 -0500

    contruyendo el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule
   ],
   providers: [],
